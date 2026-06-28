6697b9b687a5549c9b3654947669afc26608224a
chausk <chausk@gitee.com>
2026-06-28 15:53:40 +0800

    fix: MainApplication.java - remove ReactHost, fix import, public getUseDeveloperSupport

--- android/app/src/main/java/com/palmai/app/MainApplication.java
+++ android/app/src/main/java/com/palmai/app/MainApplication.java
@@ -3,11 +3,9 @@ package com.palmai.app;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactHost;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
@@ -17,7 +15,7 @@ public class MainApplication extends Application implements ReactApplication {
     private final ReactNativeHost mReactNativeHost =
         new DefaultReactNativeHost(this) {
             @Override
-            protected boolean getUseDeveloperSupport() {
+            public boolean getUseDeveloperSupport() {
                 return BuildConfig.DEBUG;
             }
 
@@ -25,7 +23,6 @@ public class MainApplication extends Application implements ReactApplication {
             protected List<ReactPackage> getPackages() {
                 @SuppressWarnings("UnnecessaryLocalVariable")
                 List<ReactPackage> packages = new PackageList(this).getPackages();
-                // Packages that cannot be autolinked yet can be added manually here
                 return packages;
             }
 
@@ -51,16 +48,10 @@ public class MainApplication extends Application implements ReactApplication {
     }
 
     @Override
-    public ReactHost getReactHost() {
-        return getDefaultReactHost(getApplicationContext(), mReactNativeHost);
-    }
-
-    @Override
     public void onCreate() {
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
             DefaultNewArchitectureEntryPoint.load();
         }
     }
